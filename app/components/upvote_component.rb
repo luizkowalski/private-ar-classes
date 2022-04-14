d5cff8a2f3a9a329b291387a93b442f0d4f29489
luizkowalski <luizeduardokowalski@gmail.com>
2022-04-14 14:59:10 +0200

    styles

--- app/components/upvote_component.rb
+++ app/components/upvote_component.rb
@@ -4,7 +4,7 @@ class UpvoteComponent < ApplicationComponent
   # `subreddit_id` is kinda not needed because
   # the upvote command fetches the post by id, ignoring the
   # subreddit_id, but we still need one for the URL generation helper to work
-  def initialize(post:, subreddit_id: 'tmp')
+  def initialize(post:, subreddit_id: 'replaceme')
     @post = post
     @subreddit_id = subreddit_id
   end
