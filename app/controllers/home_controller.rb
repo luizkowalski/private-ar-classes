5d94502e42b8020faef152b147f51cea89c04399
luizkowalski <luizeduardokowalski@gmail.com>
2022-04-16 14:05:13 +0200

    bye bye upvote component

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    posts = Subreddits::Queries::FetchUserTimeline.call(user_id: current_user.id)
-
-    render(Subreddits::PostPreviewComponent.with_collection(posts))
+    @posts = Subreddits::Queries::FetchUserTimeline.call(user_id: current_user.id)
   end
 end
